c9fa5d9bce1a22cabf72161cb95348f2c8fc97e2
胡鹏 <64hp@163.com>
2018-03-17 20:22:18 +0800

    更新 koa 第二波

--- koa2/koafastfood/context.js
+++ koa2/koafastfood/context.js
@@ -2,7 +2,33 @@ const Koa = require('koa')
 const app = new Koa()
 
 async function receiver(ctx) {
-    ctx.reject();
+
+    if (ctx.url === '/' && ctx.method === 'GET') {
+
+        let html = `
+        <form method="POST" action="/">
+        <p>uername</p>
+        <input name="username"/>
+        <p>age</p>
+        <input name="age"/>
+        <p>web</p>
+        <input name="web"/>
+        
+        <button type="submit">submit</button>
+    </form>
+    `
+        ctx.body = html
+    } else if (ctx.url === '/' && ctx.method === 'POST') {
+        ctx.body = {
+            ctx
+        }
+    }
+
+    // ctx.reject();
+    // ctx.body = {
+    //     ctx
+    // }
+
 }
 
 app.use(receiver)
@@ -12,7 +38,7 @@ app.on('error', (error, ctx) => {
     console.log(error, ctx)
 })
 
-const port = 30103
+const port = 3007
 app.listen(port, () => {
     console.log(`127.0.0.1:${port} is listening`)
 })
